679eee6a8cd376b309f4ae7319698131eb8d4f9a
whitney507 <whitneymarian507@gmail.com>
2023-06-15 12:56:01 -0700

    Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * main - Entry 
  *
- * Return: Always 1 (Success)
+ * Return: Always 1
  */
 int main(void)
 {
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
