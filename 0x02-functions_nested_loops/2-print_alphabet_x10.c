93dd19a95090a4a58ad17d9620bbff2930d66379
whitney507 <whitneymarian507@gmail.com>
2023-06-20 13:21:16 -0700

    C functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,18 @@
  */
 void print_alphabet_x10(void)
 {
-	char letter;
-	int i;
-
-	for (i = 0; i < 10; i++)
-	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-	}
-
-	_putchar('\n');
+        char ch;
+        int i;
+        i = 0;
+        while (i < 10)
+        {
+                ch = 'a';
+                while (ch <= 'z')
+                {
+                        _putchar(ch);
+                        ch++;
+                }
+                _putchar('\n');
+                i++;
+        }
 }
