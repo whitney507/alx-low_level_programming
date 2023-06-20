e25ede9a54bea759c1fb5084cc28569f7fb8d5b4
whitney507 <whitneymarian507@gmail.com>
2023-06-20 02:35:17 -0700

    C functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,15 @@
 #include <unistd.h>
 
 /**
- * main - Entry point of the program
+ * main - Entry point
  *
- * Return: 0
+ * Description: Prints _putchar followed by a new line
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char c = '_';
+	write(1, "_putchar\n", 9);
 
-    write(1, &c, 1);
-    write(1, "p", 1);
-    write(1, "u", 1);
-    write(1, "t", 1);
-    write(1, "c", 1);
-    write(1, "h", 1);
-    write(1, "a", 1);
-    write(1, "r", 1);
-    write(1, '\n', 1);
-
-    return 0;
+	return (0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *main -  Prints the alphabet in lowercase followed by a new line
+ * 
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+
+	_putchar('\n');
+	 return (0);
+}
