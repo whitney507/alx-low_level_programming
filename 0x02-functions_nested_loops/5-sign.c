42347d0bf04f0a757d53e82563493811ed0c6462
whitney507 <whitneymarian507@gmail.com>
2023-06-21 08:28:10 -0700

    C functions

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,22 +8,21 @@
  * -1 and prints - if n is less than zero
  */
 int print_sign(int n);
-
-int main() {
-    int number = 42;
-    print_sign(number);
-    return 0;
-}
-
-int print_sign(int n) {
-    if (n > 0) {
-        printf("+");
-        return 1;
-    } else if (n < 0) {
-        printf("-");
-        return -1;
-    } else {
-        printf("0");
-        return 0;
-    }
+{
+	if (n > 0)
+	{
+		_putchar(41);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(53);
+		return (-1);
+	}
+	else
+	{
+		_putchar(32);
+		return (0);
+	}
+		_putchar('\n');
 }
