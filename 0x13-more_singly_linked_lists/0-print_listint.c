49fd16574ff5b99f13b734a08c0783a56a97757c
whitney507 <whitneymarian507@gmail.com>
2023-08-01 13:13:28 -0700

    more linked lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint-prints all the elements in the linked list
+ * @h : pointyer to first node in the linked list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t node = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		node++;
+		h = h->next;
+	}
+	return (node);
+}
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * listint_len - returns the number of elements in a linked list
+ * @h: pointer to first node in a linked list
+ * Return: number of nodes in a linked list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t node = 0;
+	 while (h)
+	 {
+		 node++;
+		 h = h->next;
+	 }
+	 return (node);
+}
