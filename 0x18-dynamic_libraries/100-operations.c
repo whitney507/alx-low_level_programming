7248d4409da580110a77a6264ee7801b9300ba6c
whitney507 <whitneymarian507@gmail.com>
2023-10-16 16:23:39 -0700

    updated

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,50 @@
+/**
+ * add - calculates addition
+ * @a: number
+ * @b: number
+ * Return: returns result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub -calculates substration
+ * @a: number
+ * @b: number
+ * Return: returns result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - calculates multiplication
+ * @a: number
+ * @b: number
+ * Return: return result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - calculates division
+ * @a: number
+ * @b: number
+ * Return: returns  result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - calculates mod
+ * @a: number
+ * @b: nuber
+ * Return: returns result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
